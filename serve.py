c7593ffc04321ae0746b90751b509efd5fa0c7ae
Paul Mison <paul@bynd.com>
2013-12-24 13:47:16 -0800

    Add site filtering (using raw SQL for now)

--- serve.py
+++ serve.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python
 
 from flask import Flask, escape, redirect, render_template, request, session, url_for
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import NotFound as WerkzeugNotFound
 
 import os
 import sqlite3
 
+from datetime import datetime
+
 app = Flask(__name__, static_folder='images')
 app.secret_key = "[\xf4'\n\xe3o\xfb\x86e\xf6TB\xc2\x92\xa3b\xfd\xa96|o\xd7\xfe["
-app.SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/ffffound-blech.db'
 
-db = SQLAlchemy(app)
-
-class Image(db.Model):
-    __tablename__ = 'images'
-    id = db.Column(db.String, primary_key=True)
-    url = db.Column(db.String)
-    src = db.Column(db.String)
-    orig_url = db.Column(db.String)
-    orig_src = db.Column(db.String)
-    date = db.Column(db.DateTime)
-    count = db.Column(db.Integer)
-    related = db.Column(db.String)
-    posted = db.Column(db.Boolean)
+def iso_date(value, format='medium'):
+    dt = datetime.utcfromtimestamp(value)
+    return dt.isoformat()
+app.jinja_env.filters['iso_date'] = iso_date
 
 @app.route("/")
 def index():
     offset = request.args.get('offset', 0)
     print offset
-    images = Image.query.order_by(Image.date.desc()).limit(10).offset(offset)
-    return render_template("index.html", images=images, offset=offset)
+    images = default_images(offset)
+    return render_template("index.html", images=images, site='', offset=offset)
+
+@app.route("/filter/<site>")
+def filter(site):
+    offset = request.args.get('offset', 0)
+    images = site_filter(site, offset)
+    return render_template("index.html", images=images, site=site, offset=offset)
 
 ### db queries
 
@@ -45,6 +42,15 @@ def default_images(offset=0):
     c.execute("SELECT * FROM images ORDER BY date DESC LIMIT 10 OFFSET ?", (int(offset),))
     images = c.fetchall()
     return images
+
+def site_filter(site, offset=0):
+    site = '%'+site+'%'
+    
+    c = get_connection()
+    c.execute("SELECT * FROM images WHERE orig_url LIKE ? ORDER BY date DESC LIMIT 10 OFFSET ?", (site, int(offset),))
+    images = c.fetchall()
+    return images
+    
 
 if __name__ == "__main__":
     app.run(debug = True, port=7790)
